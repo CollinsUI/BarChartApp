f001ffc62a4dde3861f96d495b9b7cd781954836
Collins Swift <91741002+CollinsUI@users.noreply.github.com>
2022-12-08 12:29:10 +0300

    Initial Commit

---
+++ BarChartApp/BarChartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarChartAppApp.swift
+//  BarChartApp
+//
+//  Created by Collins Muthomi on 08/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BarChartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
